166c1af0d9187c376072e6f8bd51253661fe1df7
anaperezzz <anamariapc060397@gmail.com>
2026-07-29 20:21:03 -0600

    Ubicacion archivo clase estudiante

---
+++ App_Estudiantes_Universitarios/App_Estudiantes_Universitarios/Estudiante.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Estudiantes_Universitarios
+{
+    public class Estudiante
+    {
+        private string _nombre;
+        private string _carnet;
+        private string _carrera;
+        private double _prom;
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+        public string Carnet
+        {
+            get { return _carnet; }
+            set { _carnet = value; }
+        }
+        public string Carrera
+        {
+            get { return _carrera; }
+            set { _carrera = value; }
+        }
+        public double Promedio
+        {
+            get { return _prom; }
+            set { _prom = value; }
+        }
+
+        public Estudiante(string nom, string carnet, string carrera, double prom)
+        {
+            _nombre = nom;
+            _carnet = carnet;
+            _carrera = carrera;
+            _prom = prom;
+        }
+
+        public bool EsDestacado()
+        {
+            return Promedio > 8.0;
+        }
+    }
+}
